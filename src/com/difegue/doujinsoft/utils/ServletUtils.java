a0bdb9b5f737754d3ac0e1fb4adfb0bc666ac7ec
Difegue <sugoi@cock.li>
2019-06-29 18:25:33 +0200

    Fix cart

--- src/com/difegue/doujinsoft/utils/ServletUtils.java
+++ src/com/difegue/doujinsoft/utils/ServletUtils.java
@@ -277,7 +277,7 @@ public class ServletUtils {
 	    queryBase += (isNameSearch || isCreatorSearch) ? " AND name LIKE ? AND creator LIKE ?" : "";
 		
 	    String query = "SELECT * " + queryBase + " ORDER BY normalizedName ASC LIMIT 15 OFFSET ?";
-	    String queryCount = "SELECT COUNT(id) " + queryBase;
+	    String queryCount = "SELECT COUNT(hash) " + queryBase;
 	    
 		PreparedStatement ret = connection.prepareStatement(query);
 		
