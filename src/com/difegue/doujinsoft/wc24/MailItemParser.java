1948dbf1d62dc4bea726b43d8c94de1a68043eda
Difegue <8237712+Difegue@users.noreply.github.com>
2024-01-30 16:24:40 +0100

    Update MailItemParser.java

--- src/com/difegue/doujinsoft/wc24/MailItemParser.java
+++ src/com/difegue/doujinsoft/wc24/MailItemParser.java
@@ -10,6 +10,7 @@ import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.ServletContext;
